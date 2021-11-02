cea41b0736ad77646d4936bed214a48913adca62
Olalekan Olonilua <oloniluaolalekan@gmail.com>
2021-11-02 16:12:46 +0100

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
     </div>
   );
 }
