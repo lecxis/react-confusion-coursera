18f45625190b9f66ec9a700ba0684154ac134e89
Olalekan Olonilua <oloniluaolalekan@gmail.com>
2021-11-03 20:12:42 +0100

    using external file to supply state and passing it as props

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes'
+import { Component } from 'react';
 
-function App() {
+class  App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      dishes:DISHES
+    };
+  }
+  render(){
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -12,9 +21,9 @@ function App() {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes}/>
     </div>
   );
 }
-
+}
 export default App;
