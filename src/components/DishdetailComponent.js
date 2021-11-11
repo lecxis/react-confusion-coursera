be2426756c42834b6566ed18b47ed84a968ba138
Olalekan Olonilua <oloniluaolalekan@gmail.com>
2021-11-11 12:34:24 +0100

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
-
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from 'react-router-dom';
 /* componentDidMount(){
         console.log('Dishdetailed component didmount invoked');
       }
@@ -55,6 +55,17 @@ function Dish(props) {
   const dish = props.dish;
   if (dish)
     return (
+      <div className="container">
+        <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
       <div className="row">
         <div className="col-12 col-md-5 m-1">
             <RenderDish dish={dish}/></div>
@@ -62,10 +73,11 @@ function Dish(props) {
         <div className="col-12 col-md-5 m-1">
           <div>
             <ul className="list-unstyled">
-              <RenderComments comment={dish.comments}/>
+              <RenderComments comment={props.comments}/>
             </ul>
           </div>
         </div>
+      </div>
       </div>
     );
   else return (<div> </div>);
