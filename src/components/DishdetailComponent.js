b32f6903b5e3e15447dffea2e7b8c5fdf2d5dbb9
Olalekan Olonilua <oloniluaolalekan@gmail.com>
2021-11-23 13:32:42 +0100

    Assignment 3.1

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem,
-  Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, Button, Row } from "reactstrap";
+  Modal, ModalHeader, ModalBody, Label, Button, Row } from "reactstrap";
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
