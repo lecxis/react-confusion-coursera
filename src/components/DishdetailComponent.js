459fdf94ab01316945f851251ed9ec7337c2b092
Olalekan Olonilua <oloniluaolalekan@gmail.com>
2021-12-27 10:28:06 +0100

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
  
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -105,6 +106,11 @@ function RenderDish({ dish }) {
   console.log("Dishdetailed component render invoked");
   if (dish)
     return (
+      <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
       <Card>
         <CardImg top src={baseUrl + dish.image} alt={dish.name} />
         <CardBody>
@@ -112,13 +118,16 @@ function RenderDish({ dish }) {
           <CardText>{dish.description}</CardText>
         </CardBody>
       </Card>
+      </FadeTransform>
     );
 }
 function RenderComments({ comment, postComment, dishId }) {
   if (comment.length >= 1) {
+   
     let comm = comment.map((comment) => {
       //let dat= new Date().toDateString();
       return (
+        <Fade in>
         <div key={comment.id}>
           <p>{comment.comment} </p>
           <p>
@@ -134,12 +143,16 @@ function RenderComments({ comment, postComment, dishId }) {
             }{" "}
           </p>
         </div>
+        </Fade>
       );
     });
+     
     return (
       <div>
         <h4> Comments</h4>
+        <Stagger in> 
         {comm}
+        </Stagger>
         <CommentForm dishId={dishId} postComment={postComment}/>
 
       </div>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle} from 'reactstrap';
   import { Loading } from './LoadingComponent';
   import { baseUrl } from '../shared/baseUrl';
+  import { FadeTransform } from 'react-animation-components';
 
   function RenderCard({item, isLoading, errMess}) {
     if (isLoading) {
@@ -17,6 +18,11 @@ import { Card, CardImg, CardText, CardBody,
     }
 
 else return(
+    <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
       <Card>
           <CardImg src={baseUrl+ item.image} alt={item.name} />
           <CardBody>
@@ -25,6 +31,7 @@ else return(
           <CardText>{item.description}</CardText>
           </CardBody>
       </Card>
+      </FadeTransform>
   );
 
 }
