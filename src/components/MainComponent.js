1a2310251e4ded53cc78e436d7d46c63bb03d515
Olalekan Olonilua <oloniluaolalekan@gmail.com>
2021-12-27 07:18:12 +0100

    React Animations

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapStateToProps = state => {
   return {
@@ -82,6 +83,8 @@ class  Main extends Component {
   return (
     <div >
         <Header/>
+        <TransitionGroup>
+        <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
         <Switch>
           <Route path='/home'component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
@@ -90,6 +93,8 @@ class  Main extends Component {
           <Route exact path='/aboutus' component={AboutUs}  />
           <Redirect to="/home" />
         </Switch>
+        </CSSTransition>
+        </TransitionGroup>
         {/*<Menu dishes={this.state.dishes} onClick={(dishId)=>this.onDishSelect(dishId)}/>
        <Dish dish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0]}/>*/}
         <Footer/>
